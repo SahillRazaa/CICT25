e219294c3f2bf584401c8abdf4418fe90e3a35c8
SahillRazaa <connectwithsahil007@gmail.com>
2025-06-03 19:46:34 +0530

    Updated ImageSlider

--- src/app/data/speakers.js
+++ src/app/data/speakers.js
@@ -17,4 +17,58 @@ export const speakers = [
       coordinator: "",
     },
   },
+  {
+    id: 2,
+    name: "Prof. Rajesh Mohan Elara",
+    photo: "/speakers/p2.jpg",
+    bio: {
+      about:
+        "Provost Chair Professor, Singapore University of Technology and Design",
+      titleOfTalk:
+        "",
+      abstract:
+        "",
+      researchInterest: [
+        "Robotics and Autonomous Systems",
+      ],
+      coordinator: "",
+    },
+  },
+  {
+    id: 3,
+    name: "Dr. Archana Kamal",
+    photo: "/speakers/p3.jpg",
+    bio: {
+      about:
+        "Associate Professor, Yale University",
+      titleOfTalk:
+        "Quantum Information Processing",
+      abstract:
+        "",
+      researchInterest: [
+        "Engineered open quantum systems",
+        "Superconducting quantum circuits",
+        "Nonlinear quantum optics and parametric cQED",
+        "Non-equilibrium dynamics in many-body systems"
+      ],
+      coordinator: "",
+    },
+  },
+  {
+    id: 4,
+    name: "Dr.Srinivas Talabattula",
+    photo: "/speakers/p4.png",
+    bio: {
+      about:
+        "Associate Professor, Indian Institute of Science Bangalore, India",
+      titleOfTalk:
+        "",
+      abstract:
+        "",
+      researchInterest: [
+        "Photonic Artificial Intelligence",
+      ],
+      coordinator: "",
+    },
+  },
 ];
